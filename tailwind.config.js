79cd5b81779f54030ef0cc1904b479331fd478ae
KiraNg <tuanna1906@gmail.com>
2023-02-13 09:26:44 +0700

    Lib | fix tailwind.config add `./styles/**/*.{js,jsx}`

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,7 @@ module.exports = {
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
     './sections/**/*.{js,ts,jsx,tsx}',
-
+    './styles/**/*.{js,jsx}',
     // Or if using `src` directory:
     './src/**/*.{js,ts,jsx,tsx}',
   ],
